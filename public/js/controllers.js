c64bea21e92df71b4af73b2ebaff5177add9ab38
andypandy <andrew.samuelsen@gmail.com>
2014-01-22 11:32:33 -0800

    updating

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,25 +7,96 @@ angular.module('myApp.controllers', []).
     //
   }]).
   controller('PlaneCtrl', ['$scope', '$http', 'Planes', 'SearchFields', function($scope, $http, Planes, SearchFields) {
-    console.log(SearchFields);
 
-
-    /*
-     *  Controllers
-     */
 
     //Search page (find all planes)
     $scope.search = function() {
       //Visible fields defaults
-      $scope.showHorsepower = true;
       $scope.searchFields = SearchFields;
+      $scope.hiddenPlanes = [];
+      $scope.hiddenPlanesPlaceholder = [];
+      $scope.showAllPlanesText = 'Show hidden planes';
+      $scope.showAllPlanes = false;
 
+
+      //Get planes from Planes service
       Planes.query(function(planes) {
-        planes = addFullFuelUsableWeight(planes);
+        //Add full fuel usable weight
+        addFullFuelUsableWeight(planes, function(updatedPlanes) {
+          planes = updatedPlanes;
 
-        $scope.planes = planes;
+          //Add usable weight
+          addUsableWeight(planes, function(updatedPlanes) {
+            $scope.planes = updatedPlanes;
+          });
+        });
       });
+
+
+      //Toggle show all items
+      $scope.toggleShowAllPlanes = function() {
+        if($scope.showAllPlanes == false) {
+          //Put items in $scope.hiddenPlanes into placeholder and empty that array
+          $scope.hiddenPlanesPlaceholder = $scope.hiddenPlanes;
+          $scope.showAllPlanesText = 'Rehide hidden planes';
+          $scope.hiddenPlanes = [];
+          $scope.showAllPlanes = true;
+        } else {
+          //Put planes back in $scope.hiddenPlanes
+          $scope.hiddenPlanes = $scope.hiddenPlanesPlaceholder;
+          $scope.showAllPlanesText = 'Show hidden planes';
+          $scope.hiddenPlanesPlaceholder = [];
+          $scope.showAllPlanes = false;
+        }
+      };
+
+
+      //Toggle an item hidden
+      $scope.toggleHidden = function(_id) {
+        //Figure out the right array to put toggled plane into
+        if($scope.showAllPlanes == false) {
+          var hiddenPlaneArray = 'hiddenPlanes';
+        } else {
+          
+          var hiddenPlaneArray = 'hiddenPlanesPlaceholder';
+        }
+
+        //
+        var index = $scope[hiddenPlaneArray].indexOf(_id);
+        console.log(index);
+        if(index == -1) {
+          $scope[hiddenPlaneArray].push(_id);
+        } else {
+          $scope[hiddenPlaneArray].splice(index, 1);
+        }
+      };
+
+
+      //Returns fullFuelUsableWeight for a plane
+      //Gross weight - weight of fuel - empty weight = full fuel usable weight
+      //weight of fuel = gallons of fuel * 6 lbs
+      function addFullFuelUsableWeight(planes, callback) {
+        planes.forEach(function(plane, i) {
+          if(plane.grossWeightPounds && plane.emptyWeightPounds && plane.fuelCapacityGallons) {
+            planes[i].fullFuelUsableWeight = (plane.grossWeightPounds-plane.emptyWeightPounds)-(plane.fuelCapacityGallons*6);
+          }
+        });
+
+        return callback(planes);
+      }
+
+      function addUsableWeight(planes, callback) {
+        planes.forEach(function(plane, i) {
+          if(plane.grossWeightPounds && plane.emptyWeightPounds) {
+            planes[i].usableWeight = plane.grossWeightPounds-plane.emptyWeightPounds;
+          }
+        });
+
+        return callback(planes);
+      }
     };
+
+
 
 
     //Create new plane (form submit handler)
@@ -39,26 +110,6 @@ angular.module('myApp.controllers', []).
           //todo
         });
     };
-
-
-
-
-    /*
-     *  Helpers
-     */
-    //Returns fullFuelUsableWeight for a plane
-    //Gross weight - weight of fuel - empty weight = full fuel usable weight
-    //weight of fuel = gallons of fuel * 6 lbs
-    function addFullFuelUsableWeight(planes) {
-      console.log(planes);
-      planes.forEach(function(plane, i) {
-        if(plane.grossWeightPounds && plane.emptyWeightPounds && plane.fuelCapacityGallons) {
-          planes[i].fullFuelUsableWeight = (plane.grossWeightPounds-plane.emptyWeightPounds)-(plane.fuelCapacityGallons*6);
-        }
-      });
-
-      return planes;
-    }
 
 
 
@@ -82,6 +133,19 @@ angular.module('myApp.controllers', []).
 
       return ret;
     };
+
+
+    //plane._id's are added to/removed from this when toggled hidden or visible
+    $scope.hiddenFilter = function(plane) {
+      var ret = true;
+
+      if($scope.hiddenPlanes.indexOf(plane._id) != -1) {
+        ret = false;
+      }
+
+      return ret;
+    };
+
 
 
 
--- public/js/services/planes.js
+++ public/js/services/planes.js
@@ -2,18 +2,87 @@ angular.module('myApp.services', []).
   service('Planes', ['$resource', function($resource) {
     return $resource('/api/v1/planes', {});
   }]).
+
+
   service('SearchFields', [function() {
-  	var searchFields = ['horsepower', 'cruiseSpeedKts'];
-  	var searchFields = [{
-  		name: 'horsepower',
-  		label: 'Horsepower',
-  		type: 'numeric',
-  		visible: true
-  	}, {
-  		name: 'cruiseSpeedKts',
-  		label: 'Cruise Speed (kts.)',
-  		type: 'numeric',
-  		visible: true
-  	}];
-  	return searchFields
+    var searchFields = ['horsepower', 'cruiseSpeedKts'];
+
+    var searchFields = [{
+      label: 'Make', //0
+      visible: true
+    }, {
+      label: 'Model', //1
+      visible: true
+    }, {
+      label: 'Model No.', //2
+      visible: true
+    }, {
+      label: 'Cruise Speed (kts.)', //3
+      visible: true
+    }, {
+      label: 'Top Speed (kts.)', //4
+      visible: false
+    }, {
+      label: 'Stall Speed Dirty (kts.)', //5
+    }, {
+      label: 'Gear', //6
+      visible: true
+    }, {
+      label: 'Mixture', //7
+      visible: true
+    }, {
+      label: 'Engine Make', //8
+      visible: true
+    }, {
+      label: 'Engine Model', //9
+      visible: true
+    }, {
+      label: 'Engine Model (Specific)', //10
+      visible: false
+    }, {
+      label: 'Horsepower', //11
+      visible: true
+    }, {
+      label: 'Gross Weight (lbs.)', //12
+      visible: true
+    }, {
+      label: 'Empty Weight (lbs.)', //13
+      visible: true
+    }, {
+      label: 'Usable Weight (lbs.)', //14
+      visible: true
+    }, {
+      label: 'Full Fuel Usable Weight (lbs.)', //15
+      visible: true
+    }, {
+      label: 'Fuel Capacity (gal.)', //16
+      visible: true
+    }, {
+      label: 'Range (nm)', //17
+      visible: true
+    }, {
+      label: 'Take Off Ground Roll (ft.)', //18
+      visible: false
+    }, {
+      label: 'Take Off Over 50 Ft. (ft.)', //19
+      visible: false
+    }, {
+      label: 'Landing Ground Roll (ft.)', //20
+      visible: false
+    }, {
+      label: 'Landing Over 50 Ft. (ft.)', //21
+      visible: false
+    }, {
+      label: 'Climb Rate (fpm.)', //22
+      visible: false
+    }, {
+      label: 'Ceiling (ft.)', //23
+      visible: false
+    }];
+
+    /*searchFields.forEach(function(field, index) {
+      searchFields[index].visible = true;
+    });*/
+
+    return searchFields
   }]);
