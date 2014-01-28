33465b7c3d9abeab59526104e54031519f3b441d
andypandy <andrew.samuelsen@gmail.com>
2014-01-28 12:17:54 -0800

    Remove create and happy

--- public/js/app.js
+++ public/js/app.js
@@ -11,12 +11,12 @@ angular.module('myApp', [
     when('/', {
     	templateUrl: 'partial/search'
     }).
-    when('/create', {
+    /*when('/create', {
     	templateUrl: 'partial/create'
     }).
     when('/happy', {
     	templateUrl: 'partial/happy'
-    }).
+    }).*/
    	otherwise({redirectTo: '/'});
 
     $locationProvider.html5Mode(true);
