31665cc1c0a9a5761a884ebae85405d033636e32
andypandy <andrew.samuelsen@gmail.com>
2014-02-07 10:28:25 -0800

    starting testing

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function(config){
+    config.set({
+    basePath : './',
+
+    files : [
+      'https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.js',
+      'http://code.angularjs.org/1.2.9/angular.min.js',
+      'http://code.angularjs.org/1.2.9/angular-route.min.js',
+      'http://code.angularjs.org/1.2.9/angular-resource.min.js',
+      'http://code.angularjs.org/1.2.9/angular-mocks.js',
+      'public/js/littleplanefinder.min.js',
+      'test/*.js'
+    ],
+
+    exclude : [],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-junit-reporter',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+})}
--- test/controllers.js
+++ test/controllers.js
@@ -1,9 +1,91 @@
-describe("Unit Testing Examples", function() {
+describe('controllers', function(){
+  var scope, controller, routeParams, httpBackend, mockPlanes;
 
-  beforeEach(angular.mock.module('myApp'));
+  beforeEach(module('myApp.controllers'));
+  beforeEach(module('myApp.services'));
+  
+  beforeEach(inject(function($rootScope, $controller, $httpBackend) {
+    scope = $rootScope.$new();
+    controller = $controller;
+    routeParams = {};
+    httpBackend = $httpBackend;
+    //mockPlanes = Planes;
+  }));
 
-  it('should have a LoginCtrl controller', function() {
-    
-    expect(App.LoginCtrl).toBeDefined();
+
+  //UserCtrl
+  describe('UserCtrl', function() {
+    var UserCtrl;
+
+    beforeEach(function() {
+      UserCtrl = controller('UserCtrl', {$scope: scope });
+    });
+
+    it('should exist', function() {
+      expect(UserCtrl).not.toBe(null);
+    });
   });
+
+
+  //PlaneCtrl
+  describe('PlaneCtrl', function() {
+    var PlaneCtrl;
+    var mockPlanes = [{make:'cessna'}];
+    var mockSearchFields = [{
+      label: 'Make',
+      field: 'make',
+      type: 'string',
+      visible: true
+    }, {
+      label: 'Cruise Speed (kts.)', //3
+      field: 'cruiseSpeedKts',
+      type: 'numeric',
+      visible: true
+    }];
+
+/*
+    beforeEach(function() {
+      
+
+      PlaneCtrl = controller('PlaneCtrl', {
+        $scope: scope,
+        $routeParams: routeParams,
+        Planes: mockPlanes,
+        SearchFields: mockSearchFields
+      });
+
+      
+    });*/
+
+    it('should exist', function() {
+      PlaneCtrl = controller('PlaneCtrl', {$scope: scope, $routeParams: routeParams, Planes: mockPlanes, SearchFields: mockSearchFields});
+      expect(PlaneCtrl).not.toBe(null);
+    });
+
+
+    it('should add filter placeholder to scope for all search fields', function() {
+      PlaneCtrl = controller('PlaneCtrl', {$scope: scope, $routeParams: routeParams, Planes: mockPlanes, SearchFields: mockSearchFields});
+
+      //if type=string, then it should be ''
+      expect(scope.make).toBe('');
+
+      //if type=numeric it should be {}
+      expect(scope.cruiseSpeedKts).toEqual({});
+    });
+
+
+    it('should have a working search()', function() {
+      httpBackend.expectGET('/api/v1/planes').respond(mockPlanes);
+      
+      PlaneCtrl = controller('PlaneCtrl', {$scope: scope, $routeParams: routeParams, Planes: mockPlanes, SearchFields: mockSearchFields});
+      
+      scope.$apply(function() {
+        scope.search();
+      });
+
+      httpBackend.flush();
+    });
+
+  });
+
 });
